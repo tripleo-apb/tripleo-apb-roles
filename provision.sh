7784b88649b773dd8a890a6f92df0fa657815346
Flavio Percoco <flaper87@gmail.com>
2017-06-27 17:04:11 +0200

    Use os-gen-config

--- provision.sh
+++ provision.sh
@@ -3,4 +3,10 @@
 service=$1
 shift
 
-docker run --rm --user root -v /root/.kube:/opt/apb/.kube tripleo-k8s-$service provision --extra-vars config_file=/opt/apb/.kube/config $*
+docker run --rm \
+       --user root \
+       -e "OPENSHIFT_TARGET=https://38.145.33.131:8443" \
+       -e "OPENSHIFT_USER=developer" \
+       -e "OPENSHIFT_PASS=developer" \
+       -v `pwd`/kube:/opt/apb/.kube \
+       tripleo-k8s-$service provision "$@"
