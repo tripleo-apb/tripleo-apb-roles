f6f892553288d76a4e292966abc54fc09f07a8d5
Flavio Percoco <flaper87@gmail.com>
2017-07-13 17:18:12 +0200

    Keystone + hieradata working

--- provision.sh
+++ provision.sh
@@ -9,4 +9,5 @@ docker run --rm \
        -e "OPENSHIFT_USER=developer" \
        -e "OPENSHIFT_PASS=developer" \
        -v `pwd`/kube:/opt/apb/.kube \
+       -v `pwd`/extra_vars.yaml:/tmp/extra_vars.yaml \
        tripleo-k8s-$service provision "$@"
